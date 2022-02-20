c49ad2c399954c77123e3471c1b9ff1515e1fa5c
liukai <2624143487@qq.com>
2022-02-20 16:16:10 +0800

    hot-fix commit1

--- src/main/java/com/liukai/springbootes/GItTest.java
+++ src/main/java/com/liukai/springbootes/GItTest.java
@@ -3,5 +3,6 @@ package com.liukai.springbootes;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
