68bc4b1e4d6aa69baca0fe8e9b0829a4f30468d0
liukai <2624143487@qq.com>
2022-02-20 16:23:25 +0800

    master commit2

--- src/main/java/com/liukai/springbootes/GItTest.java
+++ src/main/java/com/liukai/springbootes/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("master test");
     }
 }
